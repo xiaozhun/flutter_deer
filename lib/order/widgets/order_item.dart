f5167b1f4cb5f774425318cbce8a0d56966f8915
xiao-code <xiaozhun1223@gmail.com>
2025-02-10 14:56:33 +0800

    列表显示优化

--- lib/order/widgets/order_item.dart
+++ lib/order/widgets/order_item.dart
@@ -31,7 +31,6 @@ class OrderItem extends StatelessWidget {
   final int tabIndex;
   final int index;
   final Trade order;
-  
   @override
   Widget build(BuildContext context) {
 
@@ -57,21 +56,35 @@ class OrderItem extends StatelessWidget {
       children: <Widget>[
         Row(
           children: <Widget>[
-            const Expanded(
-              child: Text('15000000000（郭李）'),
+            Expanded(
+              child: Text('${order.stockCode}  ${order.tradeprice}'),
             ),
             Text(
-              '货到付款',
+              '交易时间:${order.tradedate.length>0?DateUtil.formatDateMs(int.parse(order.tradedate),format:DateFormats.y_mo_d_h_m):0}',
               style: TextStyle(
                 fontSize: Dimens.font_sp12,
-                color: Theme.of(context).colorScheme.error,
+                // color: Theme.of(context).colorScheme.error,
+              ),
+            ),
+          ],
+        ),
+        Row(
+          children: <Widget>[
+            Expanded(
+              child: Text('交易总额：${(order.tradeprice*order.tradenumber).toStringAsFixed(1)}'),
+            ),
+            Text(
+              '利润更新:${order.upprofittime.length>0?DateUtil.formatDateMs(int.parse(order.upprofittime),format:DateFormats.y_mo_d_h_m):0}',
+              style: TextStyle(
+                fontSize: Dimens.font_sp12,
+                // color: Theme.of(context).colorScheme.error,
               ),
             ),
           ],
         ),
         Gaps.vGap8,
         Text(
-          '西安市雁塔区$index',
+          '交易状态：${order.state} 交易类型：${order.tradetype}  卖单状态：${order.sellState}',
           style: Theme.of(context).textTheme.titleSmall,
         ),
         Gaps.vGap8,
@@ -81,18 +94,8 @@ class OrderItem extends StatelessWidget {
           text: TextSpan(
             style: textTextStyle,
             children: <TextSpan>[
-              const TextSpan(text: '清凉一度抽纸'),
-              TextSpan(text: '  x1', style: Theme.of(context).textTheme.titleSmall),
-            ],
-          ),
-        ),
-        Gaps.vGap8,
-        RichText(
-          text: TextSpan(
-            style: textTextStyle,
-            children: <TextSpan>[
-              const TextSpan(text: '清凉一度抽纸'),
-              TextSpan(text: '  x2', style: Theme.of(context).textTheme.titleSmall),
+              TextSpan(text: '利润值：${order.profit.toStringAsFixed(2)}  利润率: ${(order.profitrate*100).toStringAsFixed(2)}%',style: TextStyle(color: Theme.of(context).colorScheme.error,)),
+              // TextSpan(text: '  x1', style: Theme.of(context).textTheme.titleSmall),
             ],
           ),
         ),
@@ -104,48 +107,60 @@ class OrderItem extends StatelessWidget {
                 text: TextSpan(
                   style: textTextStyle,
                   children: <TextSpan>[
-                    TextSpan(text: Utils.formatPrice('20.00', format: MoneyFormat.NORMAL)),
-                    TextSpan(text: '  共3件商品', style: Theme.of(context).textTheme.titleSmall?.copyWith(fontSize: Dimens.font_sp10)),
+                    TextSpan(text: '止盈率： ${order.zhiying>0?order.zhiying:'默认'       }'),
+                    // TextSpan(text: '止损率： ${order.zhisun>0?order.zhisun:'默认'}'),
+                    // TextSpan(text: '  共3件商品', style: Theme.of(context).textTheme.titleSmall?.copyWith(fontSize: Dimens.font_sp10)),
                   ],
                 ),
               ),
             ),
-            const Text(
-              '2021.02.05 10:00',
+            Text(
+              '止损率： ${order.zhisun>0?order.zhisun:'默认'}',
               style: TextStyles.textSize12,
             ),
           ],
         ),
         Gaps.vGap8,
         Gaps.line,
+        Row(
+          children: <Widget>[
+            Expanded(
+              child: RichText(
+                text: TextSpan(
+                  style: textTextStyle,
+                  children: <TextSpan>[
+                    TextSpan(text: '订单号： ${order.tradeId}'),
+                    // TextSpan(text: '止损率： ${order.zhisun>0?order.zhisun:'默认'}'),
+                    // TextSpan(text: '  共3件商品', style: Theme.of(context).textTheme.titleSmall?.copyWith(fontSize: Dimens.font_sp10)),
+                  ],
+                ),
+              ),
+            ),
+            Text(
+              '对单号： ${order.sellId}',
+              style: TextStyles.textSize12,
+            ),
+          ],
+        ),
         Gaps.vGap8,
         Row(
           children: <Widget>[
             OrderItemButton(
               key: Key('order_button_1_$index'),
-              text: '联系客户',
+              text: '修改止盈',
               textColor: isDark ? Colours.dark_text : Colours.text,
               bgColor: isDark ? Colours.dark_material_bg : Colours.bg_gray,
-              onTap: () => _showCallPhoneDialog(context, '15000000000'),
+              onTap: () {
+                _showZhiyingDialog(context,order.tradeId);
+              },
             ),
             const Expanded(
               child: Gaps.empty,
             ),
-            OrderItemButton(
-              key: Key('order_button_2_$index'),
-              text: orderLeftButtonText[tabIndex],
-              textColor: isDark ? Colours.dark_text : Colours.text,
-              bgColor: isDark ? Colours.dark_material_bg : Colours.bg_gray,
-              onTap: () {
-                if (tabIndex >= 2) {
-                  NavigatorUtils.push(context, OrderRouter.orderTrackPage);
-                }
-              },
-            ),
             if (orderRightButtonText[tabIndex].isEmpty) Gaps.empty else Gaps.hGap10,
-            if (orderRightButtonText[tabIndex].isEmpty) Gaps.empty else OrderItemButton(
+            if (order.tradetype=='SELL') Gaps.empty else OrderItemButton(
               key: Key('order_button_3_$index'),
-              text: orderRightButtonText[tabIndex],
+              text: '卖出',
               textColor: isDark ? Colours.dark_button_text : Colors.white,
               bgColor: isDark ? Colours.dark_app_main : Colours.app_main,
               onTap: () {
@@ -190,6 +205,7 @@ class OrderItem extends StatelessWidget {
     );
   }
 
+
   void _showPayTypeDialog(BuildContext context) {
     showDialog<void>(
       context: context,
@@ -199,6 +215,57 @@ class OrderItem extends StatelessWidget {
           onPressed: (index, type) {
             Toast.show('收款类型：$type');
           },
+        );
+      },
+    );
+  }
+//   新建修改止盈弹窗
+  void _showZhiyingDialog(BuildContext context,String orderId) {
+    String _name = ''; // 定义一个变量来存储输入的值
+    showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('设置止盈止损'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: '止盈',
+                  hintText: '请输入止盈',
+                ),
+                onChanged: (value) {
+                  _name = value; // 更新_name变量的值
+                },
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: '止损',
+                  hintText: '请输入止损',
+                ),
+                onChanged: (value) {
+                  _name = value; // 更新_name变量的值
+                },
+              ),
+            ],
+          ),
+          actions: [
+            Text('订单号：$orderId'),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // 关闭对话框
+              },
+              child: Text('取消'),
+            ),
+            ElevatedButton(
+              onPressed: () => Navigator.of(context).pop(),// 关闭对话框,
+              child: Text('提交'),
+            ),
+          ],
         );
       },
     );
